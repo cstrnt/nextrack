872f5514db571d531fd5c21759d0e4fdf6b61697
Tim Raderschad <tim.raderschad@gmail.com>
2023-05-08 13:36:49 +0200

    mhh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,6 @@
 	NexTrack is an online Platform for Musicians, DJs and Radio Stations that want to be able to
 	easily get song requests.
 	<br />
-	You can see an example here
+	You can see an example here:
 </p>
 <iframe src={'/djr8der'} height="600" width="500" title="Preview" />
